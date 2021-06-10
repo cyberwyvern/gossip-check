a21edca042241df4bf4c1b9b91cac479727b8a57
Dmitriy Karpliuk <dmitriy.karpliuk@nixsolutions.com>
2021-06-10 04:58:27 +0300

    fixes

--- webapi/GossipCheck.BLL/Extensions/StringExtensions.cs
+++ webapi/GossipCheck.BLL/Extensions/StringExtensions.cs
@@ -29,7 +29,7 @@ namespace GossipCheck.BLL.Extensions
         public static string ToSentenceCase<T>(this T obj) where T : Enum
         {
             var words = Regex.Split(obj.ToString(), @"(?<=[a-z])(?=[A-Z\d])|(?<=[a-z\d])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])")
-                .Select(x => Regex.IsMatch(x, @"[A-Z]+") ? x : x.ToLower())
+                .Select(x => Regex.IsMatch(x, @"^[A-Z]+$") ? x : x.ToLower())
                 .ToList();
 
             return words[0][0].ToString().ToUpper() + string.Join(' ', words)[1..];
